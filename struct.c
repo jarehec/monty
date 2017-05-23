1db23bff4a2b14f37b0acfdcf03f183a774fd1b0
kedpak <kedpak@yahoo.com>
2017-05-23 01:11:48 +0000

    first commit

---
+++ struct.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+int _struct(char , int end_f)
+{
+	instruction_t inst[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{NULL, NULL}
+	};
+	
+	int i;
+
+	i = 0;
+	while (i <= end_f)
+	{
+		if (strcmp(
+	}
+
+}
