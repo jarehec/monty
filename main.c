48492c9048731e8bf5d698d9e109b9da1ddc76b2
kedpak <kedpak@yahoo.com>
2017-05-26 23:43:36 +0000

    fixed betty styling

--- main.c
+++ main.c
@@ -10,40 +10,33 @@ int main(int argc, char **argv)
 {
 	FILE *fp;
 	size_t file_size;
-	int ins = 0, len;
+	int ins = 0;
 	unsigned int l_num;
 	char *opc, *temp, *line = NULL;
 	stack_t *head = NULL;
-	
+
 	check_argc(argc);
 	fp = fopen(argv[1], "r");
-	if (fp== NULL)
+	if (fp == NULL)
 	{
 		printf("Error: can't open file %s\n", argv[1]);
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	l_num = 0;
 	while (getline(&line, &file_size, fp) != -1)
 	{
-		opc = strtok(line, " ");
-		len = strlen(opc);
-		if (opc[len -1] == '\n')
-		{
-			opc[len-1] = '\0';
-		} 
+		opc = strtok(line, "\n ");
 		if (opc != NULL)
 		{
 			temp = strtok(NULL, " ");
-			
 			if (temp)
 				ins = atoi(temp);
 		}
 		if (strcmp("push", opc) == 0)
-			push(&head, ins); 
+			push(&head, ins);
 		_struct(opc, &head, l_num);
 		l_num++;
 	}
-/*	pall(&head, i); */
 	if (fclose(fp) != 0)
 	{
 		free(line);
