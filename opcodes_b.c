14c4b3442cfc05923076c56a11f1c4a9f4cc2bc1
jarehec <bone1198@gmail.com>
2017-05-26 23:50:37 +0000

    added mul function

--- opcodes_b.c
+++ opcodes_b.c
@@ -84,3 +84,23 @@ void div(stack_t **h, unsigned int l_num)
 	*h = temp->next;
 	free(temp);
 }
+
+/**
+ * mul - Multiplies the first two nodes on the stack, removes first node
+ * @h: Pointer to the top of the stack
+ * @l_num: line count
+ */
+void mul(stack_t **h, unsigned int l_num)
+{
+	stack_t *temp = *h;
+
+	if (*h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't sub, stack too short\n", l_num);
+		exit(EXIT_FAILURE);
+	}
+	temp->next->n *= temp->n;
+	temp->next->prev = NULL;
+	*h = temp->next;
+	free(temp);
+}
